0fea5be698f7c5da124f2c540ec7a09c3b998e0a
MrSkyAngule <garhok124@gmail.com>
2026-03-10 11:51:30 +0300

    added SSE

--- web_apps/urls.py
+++ web_apps/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 
-from web_apps.views import dashboard
+from web_apps.views import dashboard, sse_view
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+
+    path('event/', sse_view, name='sse_view'),
 ]
--- web_apps/views.py
+++ web_apps/views.py
@@ -1,6 +1,25 @@
+from math import cos
+
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
 from commo.models import Commo
+import time
 
 def dashboard(request):
     commos = Commo.objects.order_by('-created_at')
     return render(commos, 'supder/dashboard.html', {'commos': commos})
+
+def sse_view(request):
+    def event_stream():
+        sadoifsa = Commo.objects.latest('device_id', 'data', 'created_at').id if Commo.objects.exists() else 0
+        while True:
+            new_messages = Commo.objects.filter(id__gt=sadoifsa).order_by('created_at')
+            # Формат SSE: "data: <сообщение>\n\n"
+            for msg in new_messages:
+                yield f"data: {msg.data} {msg.created_at} {msg.device_id}\n\n"
+                sadoifsa = msg.id
+
+            time.sleep(1)  # Пауза между отправками
+
+    return StreamingHttpResponse(event_stream(), content_type='text/event-stream')
+
